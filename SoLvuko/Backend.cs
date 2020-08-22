c1cac4e7862bb936e212a6fa94404f37108ee3c3
DemuirGos <59487204+DemuirGos@users.noreply.github.com>
2020-08-22 11:14:55 +0100

    there was an attempt
    the generated sodukos are not always human solvable

---
+++ SoLvuko/Backend.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoLvuko
+{
+    class SodukuCell
+    {
+        public int Value
+        { get; set; }
+        public bool isEmpty
+        { get => Value == 0; }
+        public IList<int> Possibilities
+        { get => possibilities; }
+        private static Random rng = new Random();
+        public static void Shuffle(IList<int> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+        List<int> possibilities = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public SodukuCell(int val)
+        {
+            Value = val; possibilities.Remove(val);
+            Shuffle(this.possibilities);
+        }
+    }
+    class SodukuApi
+    {
+        public List<List<SodukuCell>> sodukoBoard
+        { get; }
+        int size;
+        public SodukuApi(int size)
+        {
+            this.size = size;
+            this.sodukoBoard = new List<List<SodukuCell>>(size);
+            for (int i = 0; i < size; i++)
+            {
+                sodukoBoard.Add(new List<SodukuCell>(size));
+                for (int j = 0; j < size; j++)
+                {
+                    sodukoBoard[i].Add(new SodukuCell(0));
+                }
+            }
+        }
+        public SodukuApi(List<List<int>> list)
+        {
+            this.size = list.Count;
+            this.sodukoBoard = new List<List<SodukuCell>>(size);
+            for (int i = 0; i < size; i++)
+            {
+                sodukoBoard.Add(new List<SodukuCell>(size));
+                for (int j = 0; j < size; j++)
+                {
+                    sodukoBoard[i].Add(new SodukuCell(0));
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    sodukoBoard[i][j].Value = list[i][j];
+                }
+            }
+        }
+        public bool solve()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (sodukoBoard[i][j].isEmpty)
+                    {
+                        foreach (int value in sodukoBoard[i][j].Possibilities)
+                        {
+                            if (isCorrect(i, j, value))
+                            {
+                                sodukoBoard[i][j].Value = value;
+                                if (solve())
+                                {
+                                    return true;
+                                }
+                                else
+                                {
+                                    sodukoBoard[i][j].Value = 0;
+                                }
+                                Console.WriteLine(this.ToString());
+                            }
+                        }
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        bool isCorrect(int i, int j, int val)
+        {
+            int regionSize = (int)Math.Sqrt(size);
+            for (int k = 0; k < size; k++)
+            {
+                if (sodukoBoard[i][k].Value == val) return false;
+            }
+            for (int k = 0; k < size; k++)
+            {
+                if (sodukoBoard[k][j].Value == val) return false;
+            }
+            int regionStarty = i - i % regionSize;
+            int regionStartx = j - j % regionSize;
+            for (int k = regionStarty; k < regionStarty + regionSize; k++)
+            {
+                for (int n = regionStartx; n < regionStartx + regionSize; n++)
+                {
+                    if (k != i && n != j && sodukoBoard[k][n].Value == val) return false;
+                }
+            }
+            return true;
+        }
+        public override string ToString()
+        {
+            string s = "===========================\n";
+            for (int i = 0; i < size; i++)
+            {
+                s += "|";
+                for (int j = 0; j < size; j++)
+                {
+                    s += sodukoBoard[i][j].Value.ToString() + "|";
+                }
+                s += "\n";
+                s += "---------------------------\n";
+            }
+            s += "===========================\n";
+            return s;
+        }
+    }
+}
---
+++ SoLvuko/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SoLvuko
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        SodukuApi board;
+        int count = 23;
+        bool isGenerated = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            initializeExponenet();
+        }
+
+        void initializeExponenet()
+        {
+            board = new SodukuApi(9);
+            ShowSoduko();
+        }
+
+        private Entry GetEntry(int i ,int j)
+        {
+            int regionY = (int)(i - i % 3) / 3;
+            int regionX = (int)(j - j % 3) / 3;
+            StackLayout boardHolderObject = (StackLayout)this.FindByName("boardHolder");
+            FlexLayout lineHolder = (FlexLayout)boardHolderObject.Children[regionY];
+            Frame regionHolder = (Frame)lineHolder.Children[regionX];
+            Frame innerHolder = (Frame)regionHolder.Content;
+            StackLayout regionLineHolder = (StackLayout)innerHolder.Content;
+            int indexX = j % 3, indexY = i % 3;
+            FlexLayout Regionline = (FlexLayout)regionLineHolder.Children[indexY];
+            Entry InputBox = (Entry)Regionline.Children[indexX];
+            return InputBox;
+
+        }
+
+        private void ChangeState(bool isGen)
+        {
+            this.isGenerated = isGen;
+            if (isGenerated)
+            {
+                var genButton = (Button)this.FindByName("GenerateSoduko");
+                genButton.Text = "Solve";
+            }
+            else
+            {
+                var genButton = (Button)this.FindByName("GenerateSoduko");
+                genButton.Text = "Generate";
+            }
+            ShowSoduko();
+        }
+
+        private void GenerateSoduko_Clicked(object sender, EventArgs e)
+        {
+            if (!isGenerated)
+            {
+                ClearBord();
+                Random rnd = new Random();
+                List<int> ommit = new List<int>();
+                int countDown = count;
+                while (countDown > 0)
+                {
+                    ommit.Add(rnd.Next(1, 81));
+                    countDown--;
+                }
+                board.solve();
+                int offset = 1;
+                for (int i = 0; i < 9; i++, offset++)
+                {
+                    for (int j = 0; j < 9; j++, offset++)
+                    {
+                        if (ommit.Contains(offset)) board.sodukoBoard[i][j].Value = 0;
+                    }
+                }
+                ChangeState(true);
+            }
+            else
+            {
+                board.solve();
+                ChangeState(false);
+            }
+        }
+
+        private void ShowSoduko()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    var entryBox = GetEntry(i, j);
+                    entryBox.Text = board.sodukoBoard[i][j].isEmpty ? "" : board.sodukoBoard[i][j].Value.ToString();
+                }
+            }
+        }
+
+        private void ClearBord()
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    GetEntry(i,j).Text = "";
+            this.board = new SodukuApi(9);
+        }
+
+        private void Quit_Clicked(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            this.count = (int)e.NewValue;
+            Label diffSignal = (Label)this.FindByName("Difficulty");
+            if (count < 23)
+            {
+                diffSignal.Text = "Easy";
+            }
+            else if(count < 23 * 2)
+            {
+                diffSignal.Text = "Medium";
+            }
+            else
+            {
+                diffSignal.Text = "Hard";
+            }
+        }
+    }
+}
